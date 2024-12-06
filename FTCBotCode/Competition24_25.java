945e164600e703e095c4066c787e9d53a4406624
srgage <sarah@gagelogic.com>
2024-12-06 15:49:05 -0500

    HardwareMap to hardwareMap typo

--- FTCBotCode/Competition24_25.java
+++ FTCBotCode/Competition24_25.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.hardwareMap;
 
 
 @TeleOp(name = "FTC Competition (Blocks to Java)")
